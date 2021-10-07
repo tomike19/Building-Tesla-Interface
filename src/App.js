c6e8580bd1c94971370fcc539b2e2f48898c67c7
tomike19 <kolajoelizabeth@gmail.com>
2021-10-07 16:24:18 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from "./components/Header"
+import Home from './components/Home';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      {/* <Header /> */}
+      <Home/>
     </div>
   );
 }
