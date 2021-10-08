0f4d5ea10b69e7b32b1f49fd80a94ed290887fe4
tomike19 <kolajoelizabeth@gmail.com>
2021-10-08 11:17:45 -0700

    fixed all the images on the web page

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,11 +21,25 @@ const Home = () => {
           rightBtnText="Existing inventory"
         />
         <Section
-          title="Model 3"
+          title="Model X "
           description="Order Online for   Touchless Delivery"
           backgroundImg="model-x.jpg"
           leftBtnText="Custom order"
           rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Lowest Cost Solar Panels in America"
+          description="Money-back guarantee"
+          backgroundImg="solar-panel.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn More"
+        />
+        <Section
+          title="Solarr for New Roof"
+          description="Solar Roof Costs Less Than a New Roof"
+          backgroundImg="solar-roof.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
         />
       </Container>
     </div>
