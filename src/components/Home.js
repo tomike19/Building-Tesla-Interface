a8712f37d395117eb9f5365db01fce3090a3bd65
tomike19 <kolajoelizabeth@gmail.com>
2021-10-08 11:09:46 -0700

    Done with fixing images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
 const Home = () => {
   return (
     <div>
-      
+      <Container>
+        <Section
+          title="Model S"
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model Y"
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+        <Section
+          title="Model 3"
+          description="Order Online for   Touchless Delivery"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Custom order"
+          rightBtnText="Existing inventory"
+        />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
+ 
